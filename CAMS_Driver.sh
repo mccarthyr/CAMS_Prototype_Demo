33236756f9a04b45b54b5b7f891e73c1c055106c
mccarthyr <mccarthy.richard@gmail.com>
2017-07-01 21:31:11 +0100

    Updated driver script with new directory name for download from tag release

--- CAMS_Driver.sh
+++ CAMS_Driver.sh
@@ -4,7 +4,7 @@ tell app "Terminal"
 
 activate
 
-do script "cd Desktop/CAMS_Prototype_Demo-1.1/
+do script "cd Desktop/CAMS_Prototype_Demo-1.2/
 
 java -jar CAMS_PROTOTYPE_DEMO-1.0-SNAPSHOT-shaded.jar"
 
