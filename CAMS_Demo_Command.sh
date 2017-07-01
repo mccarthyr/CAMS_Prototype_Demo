0bfd72b9c7adbb00b8ae4b3abdce1fe6e0746650
mccarthyr <mccarthy.richard@gmail.com>
2017-07-01 21:22:30 +0100

    Updated the driver script with AppleScript and removed the demo script file

--- CAMS_Demo_Command.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-cd Desktop/CAMS_Prototype_Demo
-java -jar CAMS_PROTOTYPE_DEMO-1.0-SNAPSHOT-shaded.jar
-bash
--- CAMS_Driver.sh
+++ CAMS_Driver.sh
@@ -1,3 +1,11 @@
-#!/bin/sh
-open -a Terminal.app CAMS_Demo_Command.sh
+#!/usr/bin/osascript
 
+tell app "Terminal"
+
+activate
+
+do script "cd Desktop/CAMS_Prototype_Demo-1.0/
+
+java -jar CAMS_PROTOTYPE_DEMO-1.0-SNAPSHOT-shaded.jar"
+
+end
